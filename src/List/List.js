6645e8d57439baf167f5fe3525c103512e90f5bb
Luke Schlangen <lukeschlangen@gmail.com>
2017-10-16 18:13:30 -0500

    adding new item state

--- src/List/List.js
+++ src/List/List.js
@@ -3,15 +3,32 @@ import PropTypes from 'prop-types';
 
 class List extends Component {
 
+    constructor (props) {
+        super(props);
+        this.state = {
+            newItem: ''
+        }
+        this.newItemChange = this.newItemChange.bind(this);
+    }
+
     // Besides render there are other hooks like this
     componentDidMount () {
         console.log('List component did mount');
+    }
+
+    newItemChange (event) {
+        console.log('onChange input event for new item', event);
+        this.setState({
+            newItem: event.target.value
+        });
     }
 
     render () {
         return (
             <div>
         <h3>{this.props.listTitle}</h3>
+        <input onChange={this.newItemChange}/>
+        <div>{this.state.newItem}</div>
         <ul>
             {this.props.items.map(item => (
             <li key={item.title}> {/* key must be unique, used for internal react tracking */}
